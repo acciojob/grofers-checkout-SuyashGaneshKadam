b546313d04b12b417deedf9e16aec8413cf899b2
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-25 19:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum = 0;
+	let prices = document.querySelectorAll(".price");
+	for(let i=0 ; i<prices.length ; i++){
+		sum += parseInt(prices[i].innerText);
+	}
+	// console.log(sum);
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	td.innerText = sum;
+	tr.append(td);
+	let table = document.querySelector("table");
+	table.append(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
